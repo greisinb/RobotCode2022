a98afb9e1afa3cb602e7c81c6a94ac1d1ca1c68a
greisinb <blake.greisinger@bremertonschools.org>
2022-03-10 10:05:11 -0800

    Add files via upload
    Basic code with lifter, launcher, and intake. Does not have climber code.

--- Robot.java
+++ Robot.java
@@ -4,26 +4,19 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
-//imports wpi and 3rd perty libaries
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; //imports library for running a differential drive
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.Joystick;// imports a library for communicating with a joystick
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import com.revrobotics.CANSparkMax; //imports library for CAN controlled SparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType; //imports library for SmarkMax control functions (required)
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
-
-
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,143 +24,107 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-//create variables, constants, and names
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-  private DifferentialDrive m_myRobot; //what to drive with the differential drive
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-  private static final int leftDeviceID = 1; 
-  private static final int rightDeviceID = 2;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
-  private RelativeEncoder m_encoder;
-
-  public double feetDriven;
-
-  public double startTime; //creates a vairable to store the time
-  //Pneumatics
-  private final DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0,1); //names the pneumatic solenoid
-  AnalogGyro gyro = new AnalogGyro(0); //creates a new analog gyroscope called gyro on analog in 0
-  public double heading; //creates a heading varibale for gyro calculations
+  private DifferentialDrive myRobot; //names a differential drive named myRobot
+  private Joystick driverStick; //names a joystick named driverStick
+  private CANSparkMax leftFrontMotor; //names CAN speed controllers named with the motor they are attached to
+  private CANSparkMax leftRearMotor;
+  private CANSparkMax rightFrontMotor;
+  private CANSparkMax rightRearMotor;
+  private MotorControllerGroup leftMotors;
+  private MotorControllerGroup rightMotors;
+  private CANSparkMax intakeMotor;
+  private CANSparkMax lifterMotor;
+  private CANSparkMax shooterMotor;
+  public Double startTime;
+  
+  DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1); //creates double solenoid (solenoid2) on output 0 and 1
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
-  //Instantiates hardware. Connects names and variables from above to specific instances of hardware
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-
-    m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
-
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor); //creates a differential drive to control my_robot
-
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
-
-    m_encoder = m_rightMotor.getEncoder(); //attaches the encoder value from right motor to the variable m_encoder
-
-   
-    Shuffleboard.getTab("Example tab").add(gyro); //creates a gyro tab on the shuffleboard
+    leftFrontMotor = new CANSparkMax(1, MotorType.kBrushless); //creates a CAN speed controller for leftFrontMotor w/CAN ID #1 set for brushless mode
+    leftRearMotor = new CANSparkMax(2, MotorType.kBrushless);
+    rightFrontMotor = new CANSparkMax(4, MotorType.kBrushless);
+    rightRearMotor = new CANSparkMax(3, MotorType.kBrushless);
+    leftMotors = new MotorControllerGroup(leftFrontMotor, leftRearMotor); //groups left motors together as a slingle object called left motors
+    rightMotors = new MotorControllerGroup(rightFrontMotor, rightRearMotor);
+    rightMotors.setInverted(true);
+    myRobot = new DifferentialDrive(leftMotors, rightMotors);
+    driverStick = new Joystick(0); //creates a new joystick on USB input 0
+    intakeMotor = new CANSparkMax(5, MotorType.kBrushed);
+    lifterMotor = new CANSparkMax(6, MotorType.kBrushless);
+    shooterMotor = new CANSparkMax(7, MotorType.kBrushless);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() {} //always running regardless of robbot mode
+  public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-    startTime = Timer.getFPGATimestamp(); //sets the auto start time
-heading=gyro.getAngle();
+    startTime = Timer.getFPGATimestamp();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-        
+    double time = Timer.getFPGATimestamp();
+    if(time - startTime < 2){
+      myRobot.arcadeDrive(-0.5, 0);
     }
-    double error = gyro.getAngle();
-        m_myRobot.tankDrive(.5 + (.005 * error), .5 - (.005 * error));
+    else {
+      myRobot.arcadeDrive(0, 0);
+    }
   }
 
-  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-   m_rightMotor.getEncoder().setPosition(0); //resets encoder between teleops
-
- }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY()); //drive my_robot with a TankDrive style getting the values from sticks
-    if (m_leftStick.getRawButtonPressed(1)){ //upon pressing button 1 on the left stick do this action
-    solenoid.set(DoubleSolenoid.Value.kForward);
-    }
-    if (m_leftStick.getRawButtonReleased(1)){ //when button is released do this action
-      solenoid.set(DoubleSolenoid.Value.kReverse);
-    }
-    feetDriven = (2.0*3.14*3*(1/10.71)*m_encoder.getPosition()/12); //computes feet driven from encoder revolutions
-    SmartDashboard.putNumber("Encoder Velocity1", m_encoder.getVelocity()); //output the encoder value in RPM to the smart dashboard
-    SmartDashboard.putNumber("Drive Distance 1", feetDriven);
+    intakeSolenoid.set(Value.kReverse); //brings up the intake bar at teleop start
   }
 
-  /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void teleopPeriodic() {
+    if(driverStick.getRawButtonPressed(2)) {
+      intakeMotor.set(-0.5);
+      intakeSolenoid.set(Value.kForward); //sets solenoid in forward position, can also set for kOff and kReverse
 
-  /** This function is called periodically when disabled. */
+    }
+    if(driverStick.getRawButtonReleased(2)){
+      intakeMotor.set(0);
+      intakeSolenoid.set(Value.kReverse); //sets solenoid in reverse position, can also set for kOff and kForward
+
+    }
+    if (driverStick.getRawButtonPressed(1)){
+      lifterMotor.set(0.3);
+      shooterMotor.set(0.8);
+    }
+    if(driverStick.getRawButtonReleased(1)){
+      lifterMotor.set(0);
+      shooterMotor.set(0);
+    }
+    myRobot.arcadeDrive(-driverStick.getY(), driverStick.getX());
+
+  }
+
   @Override
-  public void disabledPeriodic() {}
+public void disabledInit() {}
+  
 
-  /** This function is called once when test mode is enabled. */
+  @Override
+  public void disabledPeriodic() {
+
+  }
+
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
 }
